844730b8849c9fb798c4cffd6ca017dc23633182
LENOVO <2625858661@qq.com>
2021-08-28 08:17:45 +0800

    master111

---
+++ src/main/java/com/example/demo1111/Demo1111Application.java
@@ -0,0 +1,13 @@
+package com.example.demo1111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1111Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo1111/Demo1111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo1111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
